0e0ca098f341a920e4a6faecbecc290ff2915a8b
Juliane Moura <mbcjuliane@gmail.com>
2025-11-08 12:42:55 -0300

    Aula do dia 08-11

--- Aula10/frontend/js/cadastro.js
+++ Aula10/frontend/js/cadastro.js
@@ -9,16 +9,18 @@ const maior_id = () => {
     })
 }
 const gravar = () => {
+    // Inibir o botão de gravar e reset para o usuário não clicar enquanto grava
+    document.getElementById("modal-cadastro").classList.add("show");
     // Buscando os dados do formulário
     const id = document .querySelector("#id").value;
     const descricao = document .querySelector("#iddesc").value;
-    const saldo = document .querySelector("#idsaldo").value;
-    const preco = document .querySelector("#idpreco").value;
+    const saldo = parseInt(document .querySelector("#idsaldo").value);
+    const preco = parseFloat(document .querySelector("#idpreco").value);
 
-    // Montando o JSON para gravar dados
+    // Montando o JSON para gravar dados - comando rodando em segundo plano
     maior_id().then((ret)=> {
         const dados = {
-        "id": ( id == "null" ? ret+1 : id),
+        "id": ""+( id == "null" ? parseInt(ret) + 1 : parseInt(id)),
         "descricao": descricao,
         "saldo": saldo,
         "preco": preco,
@@ -34,15 +36,31 @@ const gravar = () => {
     }
     })
 }
-// Colocando os eventos no formulário
+const carregar = async () => {
+    const parametros = new URLSearchParams(window.location.search);
+    const id = parametros.get("id");
+    if (id){
+        document.getElementById("id").value = id;
+        const res = await axios.get("http://localhost:3000/produtos/"+id);
+        document.getElementById("iddesc").value = res.data.descricao;
+        document.getElementById("idpreco").value = res.data.preco;
+        document.getElementById("idsaldo").value = res.data.saldo;
+    }
+}
+// Colocando os eventos no formulário   
 const form = document.querySelector('form');
 form.addEventListener('submit',function (e){
     // Tirar a submissão do form
     e.preventDefault();
     // Chamando a gravação do registro
     gravar();
-    // Voltando para a página inicial
+    // Voltando para a página inicial - precisa colocar pausa para não impactar os demais comandos que estão rodando em paralelo
     setTimeout (()=> {
         window.location.href = "index.html";
     },3000);
+})
+
+// Colocando um documento de carregamento da página
+document.addEventListener("DOMContentLoaded",function(){
+    carregar();
 })
--- Aula10/frontend/js/index.js
+++ Aula10/frontend/js/index.js
@@ -8,12 +8,14 @@ const carregar = () => {
             const td_desc = document.createElement('td');
             const td_saldo = document.createElement('td');
             const td_preco = document.createElement('td');
+            const td_acoes = document.createElement('td');
 
             // Adicionando os TDs nos TRs
             tr.appendChild(td_id);
             tr.appendChild(td_desc);
             tr.appendChild(td_saldo);
             tr.appendChild(td_preco);
+            tr.appendChild(td_acoes);
 
             // Adicionando ao Tbody
             tbbody.appendChild(tr)
@@ -22,7 +24,19 @@ const carregar = () => {
             td_desc.textContent = item.descricao;
             td_saldo.textContent = item.saldo;
             td_preco.textContent = item.preco;
+            td_acoes.innerHTML=`<a href="cadastro.html?id=${item.id}">
+            <img src="images/edit.svg" class="img-edit"></a> | 
+            <img src="images/delete.svg" class="img-delete" onclick="apagar(${item.id});">`
             })
         })
+}
+const apagar = async(id) => {
+    const op = confirm("Deseja mesmo excluir o registro " + id + " ?");
+    if (op) {
+        await axios.delete("http://localhost:3000/produtos/"+id);
+        window.location.reload();
+    } else {
+        console.log("Cancelei a deleção!");
+    }
 }
 carregar();
